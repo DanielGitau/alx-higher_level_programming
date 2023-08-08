e0f87241d57ee0443ee272e39ab0a4807f89391f
DanielGitau <danielgitau3396@gmail.com>
2023-08-08 14:05:06 -0700

    Last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+
+def classify_ls(number):
+    digit = abs(number) % 10
+    if number < 0:
+        digit = -digit
+
+    if digit > 5:
+        return "greater than 5"
+    elif digit == 0:
+        return "0"
+    else:
+        return "less than 6 and not 0"
+
+number = random.randint(-10000, 10000)
+print("Last digit of {} is {} and is {}".format(number, abs(number) % 10, classify_ls(number)))
